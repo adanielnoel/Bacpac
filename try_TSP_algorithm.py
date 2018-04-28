dc88a476d0876aa53eb231a648a789c85b58001b
AlbertoBonifazi <albertobonifazi@outlook.com>
2018-04-28 23:14:14 +0200

    trial tsp algorithm

--- try_TSP_algorithm.py
+++ try_TSP_algorithm.py
@@ -1,9 +1,20 @@
 import numpy as np
 from greedy_TSP_albi import solve_tsp
+from main import dataset
+from utils import transport_cost
+'''#test
 #a = np.array([[0,2,3],[7,0,5], [9,10,0]])
 a = np.random.rand(14,14)
 np.fill_diagonal(a, 0)
+print(a)'''
+a = np.zeros((14,14))
+
+for i, city1 in enumerate(dataset.index.values):
+    for j, city2 in enumerate(dataset.index.values):
+        a[i, j] = transport_cost(dataset, city1, city2)
+
 print(a)
+
 def array2list(array):
     'list conversions'
     b = list(array)
@@ -12,5 +23,6 @@ def array2list(array):
     return triangular_list
 
 D= array2list(a)
+print(D)
 path = solve_tsp( D )
 print(path)
